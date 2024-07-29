98cdbe514b78571f1b92a348f82d642a14defa88
Vi Nguyen <vi.nguyenvan@axonactive.com>
2024-07-29 17:07:24 +0700

    reset

--- dev.cmd
+++
@@ -1,2 +0,0 @@
-echo off
-bun run dev
--- src/components/hoc1/MyComponentWithLoading.tsx
+++
@@ -1,6 +0,0 @@
-import MyComponent from "./MyComponent";
-import withLoading from "./withLoading.hoc";
-
-const MyComponentWithLoading = withLoading(MyComponent);
-
-export default MyComponentWithLoading;
