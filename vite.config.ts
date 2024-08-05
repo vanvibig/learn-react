0fe69bfe38d58ba2bdc905af7f0653cd9b67a3e9
Vi Nguyen <vi.nguyenvan@axonactive.com>
2024-08-05 13:39:59 +0700

    init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   }
 })
