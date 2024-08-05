88fbc63d40448464a5b0cdb58726dca0eb868ae8
Vi Nguyen <vi.nguyenvan@axonactive.com>
2024-08-05 16:23:21 +0700

    center

--- src/components/templates/Template.tsx
+++ src/components/templates/Template.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default () => {
   return (
-    <div>
+    <div className="container mx-auto p-5">
       <Button>Click me</Button>
     </div>
   )
